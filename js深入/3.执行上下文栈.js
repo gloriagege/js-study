73aa9686ec2a5b3ea8c33ab22eccc94f48563c80
一蛋蛋酱 <604884064@qq.com>
2022-08-21 17:09:51 +0800

    11

---
+++ js深入/3.执行上下文栈.js
@@ -0,0 +1,32 @@
+var foo = function() {
+
+    console.log('foo1');
+
+}
+
+foo(); // foo1
+
+var foo = function() {
+
+    console.log('foo2');
+
+}
+
+foo(); // foo2
+
+
+function foo() {
+
+    console.log('foo1');
+
+}
+
+foo(); // foo2
+
+function foo() {
+
+    console.log('foo2');
+
+}
+
+foo(); // foo2
